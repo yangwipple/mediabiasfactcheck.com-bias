48f7229cace1e00ceff13f0ce9c1bbf509f3deb1
yangwipple <60417553+yangwipple@users.noreply.github.com>
2020-03-22 18:44:43 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -60,7 +60,7 @@ def get_pages() -> List[str]:
         print('# # # # # # # # # # # # # #')
         raw_html = simple_get(source)
         bs = BeautifulSoup(raw_html, 'html.parser')
-        links = bs.find('p', attrs={'style': 'text-align: center;'})
+        links = bs.find('td', attrs={'style': 'width: 542px; text-align: center;'})
         for a in links.select('a'):
             print(a['href'])
             pages.append(a['href'])
